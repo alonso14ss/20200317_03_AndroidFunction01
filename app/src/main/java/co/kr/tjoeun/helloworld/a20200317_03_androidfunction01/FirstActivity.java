dfe1ff7d19cbbd11e11d3bdd7d0a4cca2fb1b466
alonso14ss <reiyan92@gmail.com>
2020-03-17 16:10:19 +0900

    첫 액티비티 버튼과 클릭 이벤트 준비

--- app/src/main/java/co/kr/tjoeun/helloworld/a20200317_03_androidfunction01/FirstActivity.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/a20200317_03_androidfunction01/FirstActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
 
 import co.kr.tjoeun.helloworld.a20200317_03_androidfunction01.databinding.ActivityFirstBinding;
 import co.kr.tjoeun.helloworld.a20200317_03_androidfunction01.databinding.ActivityMainBinding;
@@ -16,5 +17,11 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_first);
+        binding.finishFirstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
